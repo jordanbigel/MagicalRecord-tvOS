11c1b101c75403a11fe405a6276ce8f8c11ed507
Jordan Bigel <jordan@pannacooking.com>
2017-08-15 14:47:32 -0700

    Initial checkin

---
+++ Core/MagicalRecord+Setup.h
@@ -0,0 +1,27 @@
+//
+//  MagicalRecord+Setup.h
+//  Magical Record
+//
+//  Created by Saul Mora on 3/7/12.
+//  Copyright (c) 2012 Magical Panda Software LLC. All rights reserved.
+//
+
+#import <MagicalRecordInternal.h>
+#import <MagicalRecordXcode7CompatibilityMacros.h>
+
+@interface MagicalRecord (Setup)
+
++ (void) setupCoreDataStack;
++ (void) setupAutoMigratingCoreDataStack;
+
++ (void) setupCoreDataStackWithInMemoryStore;
++ (void) setupCoreDataStackWithInMemoryStoreNamed:(MR_nonnull NSString*)storeName;
+
++ (void) setupCoreDataStackWithStoreNamed:(MR_nonnull NSString *)storeName;
++ (void) setupCoreDataStackWithAutoMigratingSqliteStoreNamed:(MR_nonnull NSString *)storeName;
+
++ (void) setupCoreDataStackWithStoreAtURL:(MR_nonnull NSURL *)storeURL;
++ (void) setupCoreDataStackWithAutoMigratingSqliteStoreAtURL:(MR_nonnull NSURL *)storeURL;
+
+
+@end
